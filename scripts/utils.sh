edb71d95d95c0c1f26cc73dff66bce44d25b0f42
Jakub Jirutka <jakub@jirutka.cz>
2023-09-14 01:34:40 +0200

    Shorten begin tag generated by update_config

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -14,20 +14,17 @@ fi
 # generated section (start/end tags and everything between) is removed.
 #
 # $1: Path of the file to modify.
-# $2: Path of the script that generated the config (just for info).
-# $3: Content to be inserted into the file; reads from STDIN if not provided.
+# $2: Content to be inserted into the file; reads from STDIN if not provided.
 update_config() {
 	local conf_file="$1"
-	local generated_by="$2"
-	local content="${3-"$(cat -)"}"  # if $3 is *not set*, read from STDIN
+	local content="${2-"$(cat -)"}"  # if $2 is *not set*, read from STDIN
 
 	local start_tag='# BEGIN generated'
 	local end_tag='# END generated'
 
 	[ -z "$content" ] || content=$(
 		cat <<-EOF
-			$start_tag by $generated_by
-			# Do not modify this block, any modifications will be lost after reboot!
+			$start_tag by one-context (do not modify this block)
 			$content
 			$end_tag
 		EOF
